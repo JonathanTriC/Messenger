d3a892a5d983c6a4c3adef0057d57472273aea2f
Jonathan Tri <jonathantric@gmail.com>
2022-10-29 21:51:23 +0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jonathan Tri Christianto on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
